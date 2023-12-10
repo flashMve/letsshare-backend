949d470023bff85d9387c5166453477e5d335f54
flashMve <abrar.javed3@gmail.com>
2023-12-10 20:36:56 +0500

    LetsShare | BackEnd

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'fq0vnlyw',
+    dataset: 'production'
+  }
+})
---
+++ schemas/index.ts
@@ -0,0 +1,13 @@
+import user from './user';
+import pin from './pin';
+import comment from './comment';
+import save from './save';
+import postedby from './postedBy';
+
+export const schemaTypes = [
+    user,
+    pin,
+    comment,
+    save,
+    postedby,
+]
